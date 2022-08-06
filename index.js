b2bc8efaae709e2c2f88f29745e11d243451a4ca
TylerIsMoore <tylernmoore07@gmail.com>
2022-08-06 15:34:31 -0400

    Adding scan-book controller

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ request('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use('/scan-book', require('./controllers/scan-book'))
+
 app.get('/', (req, res) => {
     res.send('Welcome to Tractor Depot Inc.!')
 })
