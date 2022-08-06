0dc9cebee7b5383651eee2074704c9bee27c8108
TylerIsMoore <tylernmoore07@gmail.com>
2022-08-06 15:23:35 -0400

    Adding 404 page route

---
+++ index.js
@@ -0,0 +1,13 @@
+request('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Tractor Depot Inc.!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
